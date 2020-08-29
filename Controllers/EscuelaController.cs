7dd8655ceb36bac2d7c88184f6e81dffe8221815
Raúl Fernández Fuentes <rafernandezf@hotmail.com>
2020-08-29 03:24:02 -0400

    Primera version 1.0 del proyecto Platzi Escuela MVC

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net_core.Models;
+
+namespace platzi_asp_net_core.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+            escuela.AñoDeCreación = 2005;
+            escuela.UniqueId = Guid.NewGuid().ToString();
+            escuela.Nombre = "Platzi School";
+            escuela.Ciudad = "Cochabamba";
+            escuela.Pais = "Bolivia";
+            escuela.TipoEscuela = TiposEscuela.Secundaria;
+
+            ViewBag.CosaDinamica = "La Monja";
+
+            return View(escuela);
+        }
+    }
+}
